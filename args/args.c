4913a0498db3ed0a6ba952363ae27ada122053d5
buyi97 <44101602+buyi97@users.noreply.github.com>
2018-11-29 13:17:57 -0300

    Agregado el modulo args (procesamiento de argv)

---
+++ args/args.c
@@ -0,0 +1,97 @@
+/**
+* @file args.c
+* @author buyi97
+* @date 22/11/2019
+* @brief Funciones para el procesamiento de los argumentos por linea de comandos
+*/
+
+#include "args.h"
+
+/**
+* @brief Procesa los argumentos por linea de comandos
+* @param argc : cantidad de argumentos que fueron ingresados (int)
+* @param argv : arreglo de las cadenas que fueron ingresadas (char* [])
+* @param config : puntero a config_t (config_t*)
+* @return status_t : el estado en el que termino la función (ST_OK si salio bien)
+*/
+
+status_t  proc_args(int argc, char* argv[],config_t* config){
+	
+	char* ctemp;
+	size_t ntemp;
+	status_t st = ST_OK ;
+	bool boolprot=false;
+	size_t i;
+	
+	config->help = false;
+	config->maxlen = 0;
+	
+	for(i=1;i<argc;i++){
+		if ( argv[i][0] == CHAR_ARG_LCMD_START ){
+			if( (strcmp(argv[i],STR_HELP_S)==0) || (strcmp(argv[i],STR_HELP_L)==0)){
+				config->help = true;
+				return ST_OK;
+			}
+			else if( (strcmp(argv[i],STR_NAME_S)==0 ) || (strcmp(argv[i],STR_NAME_L)==0) ){
+				if ( (i+1) < argc ){
+					strncpy(config->nombre,argv[i+1],MAX_NOMBRE);
+				}
+				else {st = ST_ERR_NO_NAME; /*Printlog*/
+				}
+			}
+
+			else if( (strcmp(argv[i],STR_PROTOCOL_S)==0) || (strcmp(argv[i],STR_PROTOCOL_L)==0) ){
+				if ( (i+1) < argc){
+					if ( strcmp(argv[i+1], STR_NMEA )  == 0 ){
+						config->protocol = P_NMEA;
+						boolprot=true;
+					}
+					else if ( strcmp(argv[i+1], STR_UBX )  == 0 ){
+						config->protocol = P_UBX;
+						boolprot=true;
+					}						
+				}
+				else {	st = ST_ERR_NO_PROTOCOL; /*Printlog*/
+				}
+			}
+			else if( (strcmp(argv[i],STR_INFILE_S)==0) || (strcmp(argv[i],STR_INFILE_L)==0) ){
+				if ( (i+1)<argc ){
+					strncpy( config->fin , argv[i+1] , MAX_NOMBRE );
+				}
+				else {	st = ST_ERR_NO_LOGFILE; /*Printlog*/
+				}
+			}
+			else if( (strcmp(argv[i],STR_OUTFILE_S) ==0) || (strcmp(argv[i],STR_OUTFILE_L)==0) ){
+				if ( (i+1)<argc ){
+					strncpy( config->fout, argv[i+1] , MAX_NOMBRE );
+				}
+				else {	st = ST_ERR_NO_LOGFILE; /*Printlog*/
+				}
+			}
+			else if( (strcmp(argv[i],STR_LOGFILE_S) ==0) || (strcmp(argv[i],STR_LOGFILE_L)==0) ){
+				if ( (i+1)<argc ){
+					strncpy( config->flog , argv[i+1] , MAX_NOMBRE );
+				}
+				else {	st = ST_ERR_NO_LOGFILE; /*Printlog*/
+				}
+			}
+			else if( (strcmp(argv[i],STR_MAXLEN_S) ==0) || (strcmp(argv[i],STR_MAXLEN_L)==0) ){
+				if ( (i+1)<argc ){
+					ntemp = strtoul( argv[i+1] , &ctemp , 10 );
+					if ( ctemp != '\0'){
+						/* st = ST_ERR_NO_MAXLEN; */ /*Printlog*/
+					}
+					else { config->maxlen = ntemp;
+					}
+				}	
+				else {	st = ST_ERR_NO_LOGFILE; /*Printlog*/
+				}
+			}
+		}
+	}
+	
+/* 	if ( boolprot == false )
+		return ST_ERR_NO_PROTOCOL; */ /*Printlog*/
+	
+	return ST_OK;
+}
---
+++ args/args.h
@@ -0,0 +1,72 @@
+/**
+* @file args.h
+* @author buyi97
+* @date 22/11/2019
+* @brief Defines para el procesamiento de los argumentos por linea de comandos
+*/
+
+#ifndef ARGS__H
+#define ARGS__H
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include "../status/status.h"
+
+#define MAX_NOMBRE 50
+
+#define PROTOCOL_CANT	2
+
+#define CHAR_ARG_LCMD_START	'-'
+
+#define STR_HELP_S "-h"
+#define STR_HELP_L "--help"
+#define STR_NAME_S "-n"
+#define STR_NAME_L "--name"
+#define STR_PROTOCOL_S "-p"
+#define STR_PROTOCOL_L "--protocol"
+#define STR_INFILE_S "-i"
+#define STR_INFILE_L "--infile"
+#define STR_OUTFILE_S "-o"
+#define STR_OUTFILE_L "--outfile"
+#define STR_LOGFILE_S "-l"
+#define STR_LOGFILE_L "--logfile"
+#define STR_MAXLEN_S "-m"
+#define STR_MAXLEN_L "--maxlen"
+
+#define STR_NMEA	"nmea"
+#define STR_UBX		"ubx"
+
+#define STR_STDIN	"-"
+#define STR_STDOUT	"-"
+#define STR_STDERR	"-"
+
+/**
+ * @enum protocol_t
+ * @brief Tipo para el protocolo de GNSS
+ */
+/*Tipo de dato para PROTOCOLO*/
+typedef enum protocol{
+	P_NMEA,
+	P_UBX,
+}protocol_t;
+
+/**
+ * @struct config_t
+ * @brief Estructura para la configuracion del programa, argumento que se le va a pasar a proc_args
+ */
+/*Estructura que le vamos a pasar a proc_argv, con las configuraciones del programa*/
+typedef struct config{
+	bool help;
+	char nombre[MAX_NOMBRE+1];
+	protocol_t protocol;
+	char fin[MAX_NOMBRE+1];
+	char fout[MAX_NOMBRE+1];
+	char flog[MAX_NOMBRE+1];
+	size_t maxlen;
+}config_t;
+
+/* Prototipos */
+status_t proc_args( int argc, char* argv[], config_t* config );
+
+#endif
