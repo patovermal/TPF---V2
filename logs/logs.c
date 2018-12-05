632884fd10c3f53c6d866576476da37260e6ac0f
patovermal <44077261+patovermal@users.noreply.github.com>
2018-12-05 18:02:29 -0300

    Update logs.c

--- logs/logs.c
+++ logs/logs.c
@@ -8,26 +8,27 @@
 
 #include "logs.h"
 
-FILE *flogs = stderr;
-
+FILE *flogs;
 
 
 status_t init_logger(char * filename){
-    if(!filename)
+    if(!filename){
         return ST_ERR_PUNT_NULL;
-  
-    if( !( flogs = fopen(filename ,"w" ) ) )
-        flogs = stderr;       
-    
+    }
+
+    if( !( flogs = fopen(filename ,"w" ) ) ){
+        flogs = stderr;
+    }
+
     return ST_OK;
 }
 
-void close_logger(){
+void close_logger(void){
         if(flogs == stderr || !flogs)
             return;
 
         fclose(flogs);
-        return;   
+        return;
 }
 
 /**
@@ -37,24 +38,25 @@ void close_logger(){
 * @return status_t : el estado en el que termina la función (si fue todo bien ST_OK)
 */
 status_t print_logs (log_t logs) {
-    
-    time_t now;     
-    time(&now);     
+
+    time_t now;
+    time(&now);
     char * date = ctime(&now);
-    
-    if (!flogs)
+
+    if (!flogs){
         return ST_ERR_PUNT_NULL;
-    
+    }
+
 	date[strlen(date) - 1] = '\0'; /* le saco el \n que tiene para que imprima todo en una linea*/
 	fprintf(flogs, "%s - " , date);
-	
+
     switch(logs) {
 
         /* Errores (ERR) */
         case ERR_OPEN_FILE:
             fprintf(flogs, "%s: %s\n", MSJ_ERR_PREFIJO, MSJ_ERR_OPEN_FILE);
 			break;
-			
+
         case ERR_INV_CHKSUM:
             fprintf(flogs, "%s: %s\n", MSJ_ERR_PREFIJO, MSJ_ERR_INV_CHKSUM);
             break;
@@ -66,35 +68,35 @@ status_t print_logs (log_t logs) {
         case ERR_INV_NMEA:
             fprintf(flogs, "%s: %s\n", MSJ_ERR_PREFIJO, MSJ_ERR_INV_NMEA);
             break;
-			
+
 		case ERR_INV_UBX:
             fprintf(flogs, "%s: %s\n", MSJ_ERR_PREFIJO, MSJ_ERR_INV_UBX);
             break;
-		
+
 		case ERR_GET_DATE:
 			fprintf(flogs, "%s: %s\n", MSJ_ERR_PREFIJO, MSJ_ERR_GET_DATE);
             break;
-			
+
 		case ERR_NO_MEM:
 			fprintf(flogs, "%s: %s\n", MSJ_ERR_PREFIJO, MSJ_ERR_NO_MEM);
             break;
-		
+
 		case ERR_LIST_MAKE:
 			fprintf(flogs, "%s: %s\n", MSJ_ERR_PREFIJO, MSJ_ERR_LIST_MAKE);
             break;
-			
+
 		case ERR_LIST_APPEND:
 			fprintf(flogs, "%s: %s\n", MSJ_ERR_PREFIJO, MSJ_ERR_LIST_APPEND);
             break;
-			
+
 		case ERR_NO_PROTOCOL:
 			fprintf(flogs, "%s: %s\n", MSJ_ERR_PREFIJO, MSJ_ERR_NO_PROTOCOL);
             break;
-		
+
 		case ERR_PROC_FILE:
 			fprintf(flogs, "%s: %s\n", MSJ_ERR_PREFIJO, MSJ_ERR_PROC_FILE);
             break;
-		
+
         /* Warnings (WARN) */
         case WARN_ID_DESC:
             fprintf(flogs, "%s: %s\n", MSJ_WARN_PREFIJO, MSJ_WARN_ID_DESC);
@@ -107,11 +109,11 @@ status_t print_logs (log_t logs) {
         case WARN_FULL_LIST:
             fprintf(flogs, "%s: %s\n", MSJ_WARN_PREFIJO, MSJ_WARN_FULL_LIST);
             break;
-			
+
 		case WARN_GPX_CONV:
             fprintf(flogs, "%s: %s\n", MSJ_WARN_PREFIJO, MSJ_WARN_GPX_CONV);
             break;
-			
+
 		case WARN_ARG_MISS:
             fprintf(flogs, "%s: %s\n", MSJ_WARN_PREFIJO, MSJ_WARN_ARG_MISS);
             break;
@@ -132,14 +134,14 @@ status_t print_logs (log_t logs) {
         case DB_MSJ_UP:
             fprintf(flogs, "%s: %s\n", MSJ_DEBUG_PREFIJO, MSJ_DB_MSJ_UP);
             break;
-		
+
 		case DB_MSJ_PRINT:
             fprintf(flogs, "%s: %s\n", MSJ_DEBUG_PREFIJO, MSJ_DB_MSJ_PRINT);
             break;
-		
+
 		case DB_DATE_ACT:
             fprintf(flogs, "%s: %s\n", MSJ_DEBUG_PREFIJO, MSJ_DB_DATE_ACT);
             break;
     }
-    return ST_OK;
+return ST_OK;
 }
