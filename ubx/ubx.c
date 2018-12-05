8c8fa62bad4ba7aaec9372dd8966dcb9dc7dfd28
patovermal <44077261+patovermal@users.noreply.github.com>
2018-12-05 17:52:41 -0300

    Se modifican los logs.
    Se saca el segundo argumento en la funcion print_logs, ya que al usar una variable global este no sera necesario.

--- ubx/ubx.c
+++ ubx/ubx.c
@@ -1,6 +1,6 @@
 /**
 * @file ubx.c
-* @Author mauroarbuello 	
+* @Author mauroarbuello
 * @date 22/11/2019
 * @brief Funciones para UBX
 */
@@ -8,13 +8,13 @@
 #include "ubx.h"
 
 /**
-* @brief imprime en formato gpx los datos leidos en formato ubx 
+* @brief imprime en formato gpx los datos leidos en formato ubx
 * @param files: estructura que contiene punteros a los flujos de entrada, salida y log
 * @param maxlen: puntero a estructura gpx
 * @return status_t : el estado en el que termina la funcion (si fue todo bien ST_OK)
 */
 status_t ubx2gpx( Files_t * files, size_t maxlen){
-	
+
 	ubx2gpx_t funcion[] ={&pvt2gpx, &tos2gpx, &posllh2gpx};
 	ubx_t * ubx;
 	bool eof = false;
@@ -34,45 +34,45 @@ status_t ubx2gpx( Files_t * files, size_t maxlen){
 	srand(time(NULL));
 
 	if ((st = get_currentdate(&fecha_cur, &hora_cur)) != ST_OK){
-		print_logs(ERR_GET_DATE, files->flog);
+		print_logs(ERR_GET_DATE);
 		return st;
 	}
-	
+
 	if ((st = Make_list(&lista, maxlen)) != ST_OK){
-		print_logs(ERR_LIST_MAKE, files->flog);
+		print_logs(ERR_LIST_MAKE);
 		return st;
 	}
 
 	if ( !(ubx = (ubx_t*) calloc(1, sizeof(ubx_t))) ){
-		print_logs( ERR_NO_MEM, files->flog );
+		print_logs( ERR_NO_MEM);
 		return ST_ERR_NOMEM;
 	}
 
 	while(!eof){
-		
-		print_logs( DB_BYTES_SYNC , files->flog ) ;
+
+		print_logs( DB_BYTES_SYNC ) ;
 		readline_ubx(&sentencia, &eof, files->fin);
 
-		if ((st = proc_ubx(sentencia, ubx , files->flog)) != ST_OK){
-			print_logs( ERR_INV_UBX , files->flog );
+		if ((st = proc_ubx(sentencia, ubx)) != ST_OK){
+			print_logs( ERR_INV_UBX );
 			continue;
 		}
 
-		print_logs( DB_MSJ_DET , files->flog );
-		
+		print_logs( DB_MSJ_DET );
+
 		switch ( ubx->id ){
 			case NAV_PVT:
 
 				fecha_cur = ubx->type.pvt.fecha;
 				hora_cur = ubx->type.pvt.hora;
-				print_logs( DB_DATE_ACT , files->flog );
+				print_logs( DB_DATE_ACT );
 				break;
 
 			case TIM_TOS:
-				
+
 				fecha_cur = ubx->type.tim_tos.fecha;
 				hora_cur = ubx->type.tim_tos.hora;
-				print_logs( DB_DATE_ACT , files->flog );
+				print_logs( DB_DATE_ACT );
 				continue;
 
 			case NAV_POSLLH:
@@ -83,7 +83,7 @@ status_t ubx2gpx( Files_t * files, size_t maxlen){
 				continue;
 		}
 
-		
+
 		if (!(gpx = (gpx_t*) calloc( 1, sizeof(gpx_t)))){
 			free(ubx);
 			ubx = NULL;
@@ -94,21 +94,21 @@ status_t ubx2gpx( Files_t * files, size_t maxlen){
 		if ( ( st = funcion[ubx->id]( ubx, gpx)) != ST_OK ) {
 			free(gpx);
 			gpx = NULL;
-			print_logs( WARN_GPX_CONV , files->flog );
+			print_logs( WARN_GPX_CONV );
 			continue;
 		}
-		
+
 		gpx->fecha = fecha_cur;
 		gpx->hora = hora_cur;
 
 		if ((st = AppendR_list( &lista, gpx)) != ST_OK){
-			print_logs(st, files->flog);
+			print_logs(st);
 			free(gpx);
 			gpx = NULL;
 		}
 
-		print_logs( DB_MSJ_UP , files->flog );
-		
+		print_logs( DB_MSJ_UP );
+
 		if ( rand()%10 <= 7 ){
 			print_trkptGPX( PopL_list( &lista ) ,files->fout);
 			Destroy_firstnode( &lista, &free );
@@ -119,14 +119,14 @@ status_t ubx2gpx( Files_t * files, size_t maxlen){
 		print_trkptGPX( PopL_list( &lista), files->fout);
 		Destroy_firstnode( &lista, &free);
 	}
-	
+
 	free(ubx);
 	Destroy_list( &lista , &free );
 	return ST_OK;
 }
 
 /**
-* @brief Carga en una estructura gpx_t los datos de una sentencia pvt 
+* @brief Carga en una estructura gpx_t los datos de una sentencia pvt
 * @param ubx: puntero a estructura ubx
 * @param gpx: puntero a estructura gpx
 * @return status_t : el estado en el que termina la funcion (si fue todo bien ST_OK)
@@ -147,14 +147,14 @@ status_t pvt2gpx( ubx_t * ubx, gpx_t * gpx){
 }
 
 /**
-* @brief Carga en una estructura gpx_t los datos de una sentencia posllh 
+* @brief Carga en una estructura gpx_t los datos de una sentencia posllh
 * @param ubx: puntero a estructura ubx
 * @param gpx: puntero a estructura gpx
 * @return status_t : el estado en el que termina la funcion (si fue todo bien ST_OK)
 */
 status_t tos2gpx( ubx_t* ubx , gpx_t* gpx){/*se utilizaba en una implementacion anterior y se conserva en caso de ser necesaria en futuras implementaciones*/
 
-	
+
 	if ( !ubx || !gpx )
 		return ST_ERR_PUNT_NULL;
 
@@ -168,7 +168,7 @@ status_t tos2gpx( ubx_t* ubx , gpx_t* gpx){/*se utilizaba en una implementacion 
 }
 
 /**
-* @brief Carga en una estructura gpx_t los datos de una sentencia posllh 
+* @brief Carga en una estructura gpx_t los datos de una sentencia posllh
 * @param ubx: puntero a estructura ubx
 * @param gpx: puntero a estructura gpx
 * @return status_t : el estado en el que termina la funcion (si fue todo bien ST_OK)
@@ -194,7 +194,7 @@ status_t posllh2gpx( ubx_t* ubx , gpx_t* gpx){
 * @param ubx: puntero a estructura ubx
 * @return status_t : el estado en el que termina la funcion (si fue todo bien ST_OK)
 */
-status_t proc_ubx(uchar * sentencia, ubx_t * ubx, FILE* flog){
+status_t proc_ubx(uchar * sentencia, ubx_t * ubx){
 	proc_ubx_t funcion[] = {&proc_nav_pvt, &proc_tim_tos, &proc_nav_posllh};
 
 	if ( !sentencia || !ubx )
@@ -204,21 +204,21 @@ status_t proc_ubx(uchar * sentencia, ubx_t * ubx, FILE* flog){
 	switch(letol(sentencia, ID_POS, ID_LEN)){
 		case UBX_NAV_PVT_ID:
 			ubx->id = NAV_PVT;
-			print_logs( DB_ID_DETECT , flog );
+			print_logs( DB_ID_DETECT);
 			break;
 
 		case UBX_TIM_TOS_ID:
-			print_logs( DB_ID_DETECT , flog );
+			print_logs( DB_ID_DETECT);
 			ubx->id = TIM_TOS;
 			break;
 
 		case UBX_NAV_POSLLH_ID:
-			print_logs( DB_ID_DETECT , flog );
+			print_logs( DB_ID_DETECT );
 			ubx->id = NAV_POSLLH;
 			break;
 
-		default:	
-			print_logs( WARN_ID_DESC , flog );
+		default:
+			print_logs( WARN_ID_DESC );
 			return ST_ERR_ID_INVALIDO;
 	}
 
@@ -244,12 +244,12 @@ status_t proc_nav_pvt(const uchar * payload, ubx_t * ubx){
 	ubx->type.pvt.latitud =  UBX_INT_SCALE*msletol(payload,UBX_PVT_LATITUD_POS ,UBX_PVT_LATITUD_LEN);
 	ubx->type.pvt.longitud = UBX_INT_SCALE*msletol(payload,UBX_PVT_LONGITUD_POS ,UBX_PVT_LONGITUD_LEN);
 	ubx->type.pvt.elevacion = msletol(payload, UBX_PVT_ELEVACION_POS, UBX_PVT_ELEVACION_LEN);
-	
+
 	/*carga la fecha*/
 	ubx->type.pvt.fecha.year = (int) letol(payload, UBX_PVT_YEAR_POS, UBX_PVT_YEAR_LEN);
 	ubx->type.pvt.fecha.month = (int) letol(payload, UBX_PVT_MONTH_POS, UBX_PVT_MONTH_LEN);
 	ubx->type.pvt.fecha.day = (int) letol(payload, UBX_PVT_DAY_POS, UBX_PVT_DAY_LEN);
-	
+
 	/*carga la hora*/
 	ubx->type.pvt.hora.hh = (int) letol(payload, UBX_PVT_HH_POS, UBX_PVT_HH_LEN);
 	ubx->type.pvt.hora.mm = (int) letol(payload, UBX_PVT_MM_POS, UBX_PVT_MM_LEN);
@@ -273,10 +273,10 @@ status_t proc_tim_tos(const uchar * payload, ubx_t * ubx){
 	ubx->type.tim_tos.fecha.year = (int) letol(payload, UBX_TIM_TOS_YEAR_POS, UBX_TIM_TOS_YEAR_LEN);
 	ubx->type.tim_tos.fecha.month = (int) letol(payload, UBX_TIM_TOS_MONTH_POS, UBX_TIM_TOS_MONTH_LEN);
 	ubx->type.tim_tos.fecha.day = (int) letol(payload, UBX_TIM_TOS_DAY_POS, UBX_TIM_TOS_DAY_LEN);
-	
+
 	/*carga la hora*/
 	ubx->type.tim_tos.hora.hh = (int) letol(payload, UBX_TIM_TOS_HH_POS, UBX_TIM_TOS_HH_LEN);
-	ubx->type.tim_tos.hora.mm = (int) letol(payload, UBX_TIM_TOS_MM_POS, UBX_TIM_TOS_MM_LEN); 
+	ubx->type.tim_tos.hora.mm = (int) letol(payload, UBX_TIM_TOS_MM_POS, UBX_TIM_TOS_MM_LEN);
 	ubx->type.tim_tos.hora.ss = (int) letol(payload, UBX_TIM_TOS_SS_POS, UBX_TIM_TOS_SS_LEN);
 
 	return ST_OK;
@@ -328,25 +328,25 @@ status_t readline_ubx(uchar ** sentencia, bool * eof, FILE * fin){
 		/*completa el final del buffer con ceros si no hay mas datos para leer del archivo*/
 		if(feof(fin)){
 			for(i = leido ; i < BUFFER_LEN ; i++){
-				buffer[i] = 0; 
+				buffer[i] = 0;
 			}
 		}
-		buffer_empty = false;	
+		buffer_empty = false;
 	}
 
 	*eof = false;
 
 	/*busca y devuelve sentencias UBX validadas hasta llegar a EOF*/
-	while(get_sentence(buffer, fin)){ 
+	while(get_sentence(buffer, fin)){
 		if(checksum(buffer)){
 			/*IMPRIMIR LOG*/
 			*sentencia = buffer;
 			return ST_OK;
 		}else{
-			/*IMPRIMIR LOG*/ 
+			/*IMPRIMIR LOG*/
 		}
 	}
-	
+
 	/*si salio del while es porque no hay mas sentencias*/
 	*sentencia = NULL;
 	*eof=true;
@@ -364,15 +364,15 @@ bool get_sentence(uchar * buffer, FILE * fin){
 
 	/*busca los dos caracteres de sincronismo en el buffer excepto en los dos ultimos bytes*/
 	for(i = 0 ; i < BUFFER_LEN-2 ; i++){
-		if(buffer[i] == SYNC_CHAR1){ 
-			if(buffer[i + 1] == SYNC_CHAR2){ 
-				
+		if(buffer[i] == SYNC_CHAR1){
+			if(buffer[i + 1] == SYNC_CHAR2){
+
 				/*IMPRIMIR LOG caracteres de sincronismo encontrados*/
 				/*mueve la sentencia al principio del buffer*/
 				load_buffer(buffer, i + 2, fin);
 				return true;
-			}	
-		}		
+			}
+		}
 	}
 
 	/*si salio del 'for' y se termino el archivo es porque no hay mas sentencias para leer*/
@@ -382,7 +382,7 @@ bool get_sentence(uchar * buffer, FILE * fin){
 
 	/*si salio del 'for' y no se termino el archivo mueve los dos ultimos bytes al principio del buffer y vuelve a empezar*/
 	load_buffer(buffer, BUFFER_LEN-2, fin);
-	return get_sentence(buffer, fin);	
+	return get_sentence(buffer, fin);
 }
 
 /**
@@ -404,15 +404,15 @@ void load_buffer(uchar * buffer, size_t pos, FILE * fin){
 
 	/*sobreescribe la parte final del buffer*/
 	if((leido = fread_grind(buffer + BUFFER_LEN - pos, 1, pos, fin)) != pos){
-		
+
 		/*completa el final del buffer con ceros si no hay mas datos para leer del archivo*/
-		if(feof(fin)){ 	
+		if(feof(fin)){
 			for(i = BUFFER_LEN - pos + leido ; i < BUFFER_LEN ; i++){
 				buffer[i] = 0;
-			} 
+			}
 		}
-	}	
-	
+	}
+
 	return;
 }
 
@@ -426,12 +426,12 @@ void load_buffer(uchar * buffer, size_t pos, FILE * fin){
 */
 size_t fread_grind(void *ptr, size_t size, size_t nmemb, FILE *stream){
 	size_t leido;
-	
-	if((leido = fread(ptr, size, nmemb, stream)) != nmemb){ 
+
+	if((leido = fread(ptr, size, nmemb, stream)) != nmemb){
 			if (ferror(stream)){
 				/*IMPRIMIR LOG*/
 			}
-			if(feof(stream)){ 
+			if(feof(stream)){
 				/*IMPRIMIR LOG*/
 			}
 	}
@@ -440,7 +440,7 @@ size_t fread_grind(void *ptr, size_t size, size_t nmemb, FILE *stream){
 
 /**
 * @brief calcula el checksum
-* @param sentencia: sentencia en la que se va a calcular el  checksum 
+* @param sentencia: sentencia en la que se va a calcular el  checksum
 * @return bool : true si el checksum concuerda
 */
 bool checksum(const uchar *sentencia){
@@ -448,13 +448,13 @@ bool checksum(const uchar *sentencia){
 	      ck_b = 0;
 	long largo = 0;
 	int i;
-	
+
 	/*lee el largo*/
 	largo = letol(sentencia, LARGO_POS, LARGO_LEN);
-	
+
 	/*si el largo leido es mayor al espacio disponible en el buffer no se puede realizar la lectura*/
 	if(largo > BUFFER_LEN - ID_LEN - LARGO_LEN - CHECKSUM_LEN){
-		/*IMPRIMIR LOG: se encontró y descartó sentencia de tamaño mayor al buffer*/
+		/*IMPRIMIR LOG*/
 		return false;
 	}
 
