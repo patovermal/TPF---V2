a3265de0944ea03709d58a265ccdb3224b2f6c30
buyi97 <44101602+buyi97@users.noreply.github.com>
2018-12-05 21:17:22 -0300

    Actualizado main.c, ahora imprime por stderr

--- main.c
+++ main.c
@@ -22,99 +22,131 @@ int main(int argc, char* argv[]){
 
 	strncpy( config.nombre , DEFAULT_NOMBRE , MAX_NOMBRE );
 	config.nombre[MAX_NOMBRE] = '\0';
-
+	
+	init_logger(NULL) ; /* inicializa a stderr como archivo de log */
+	
+	/* proceso de argumentos por linea de comandos */
 	if ( ( st = proc_args( argc , argv , &config ) ) != ST_OK  ){
-		fprintf(stderr, "%s\n", MSJ_ERR_OPEN_FILE);
+		print_logs( ERR_PROC_ARGS );
 		return EXIT_FAILURE;
 	}
-
+	/* fin proceso de argumentos por linea de comandos */
+	
+	/* comprobacion de comando de ayuda activado */
 	if ( config.help == true ){
 		print_help();
 		return EXIT_SUCCESS;
 	}
-
-	if ( !config.bool_prot){
-		fprintf(stderr, "%s\n", MSJ_ERR_NO_PROTOCOL);
-		return EXIT_FAILURE;
-	}
-
-	if ( !config.maxlen ){
-		config.maxlen = MAX_LEN_DEFAULT;
-	}
-
+	/* fin comprobacion de comando de ayuda activado */
+	
+	
 	/* apertura de archivos */
+	
+		/* log */
+	if ( strcmp( config.flog , STR_STDERR ) && config.bool_flog && ( init_logger(config.flog) ) != ST_OK ) {
+		print_logs( ERR_OPEN_FILE );
+		return EXIT_FAILURE;
+	}	
+	
+		/* in */
 	if ( !strcmp( config.fin , STR_STDIN ) || !config.bool_fin ){
 		files.fin = stdin ;
 	}
 	else if ( config.protocol == P_NMEA ){
 		if ( !(files.fin = fopen( config.fin , "r" ) ) ){
-			fprintf(stderr, "%s\n", MSJ_ERR_OPEN_FILE);
+			print_logs( ERR_OPEN_FILE );
+			
+			/*cierre de archivos*/
+			close_logger();
+			
 			return EXIT_FAILURE;
 		}
 	}
 	else if ( config.protocol == P_UBX ){
 		if ( !(files.fin = fopen( config.fin , "rb" ) ) ){
-			fprintf(stderr, "%s\n", MSJ_ERR_OPEN_FILE);
+			print_logs( ERR_OPEN_FILE );
+			
+			/*cierre de archivos*/
+			close_logger();
+			
 			return EXIT_FAILURE;
 		}
 	}
 	else {
-		fprintf(stderr, "%s\n", MSJ_ERR_NO_PROTOCOL);
+		print_logs( ERR_NO_PROTOCOL );
+		
+		/*cierre de archivos*/
+		close_logger();
+		
+		return EXIT_FAILURE;
 	}
-
+	
+		/* out */
 	if (!strcmp( config.fout , STR_STDOUT ) || !config.bool_fout ){
 		files.fout = stdout ;
 	}
 	else if ( !(files.fout = fopen( config.fout , "w" ) ) ){
-		fprintf(stderr, "%s\n", MSJ_ERR_OPEN_FILE);
+		print_logs( ERR_OPEN_FILE );
+		
+		/*cierre de archivos*/
+		if ( files.fin != stdin )
+			fclose( files.fin );
+		close_logger();
+		
 		return EXIT_FAILURE;
 	}
-	if (!strcmp( config.flog , STR_STDERR ) || !config.bool_flog ){
-		files.flog = stderr ;
-	}
-
-	else if ( ( init_logger(config.flog) ) != ST_OK ){
-		fprintf(stderr, "%s\n", MSJ_ERR_OPEN_FILE);
-		return EXIT_FAILURE;
-	}
-
 	/* fin de apertura de archivos */
+
+	if ( !config.maxlen ){
+		config.maxlen = MAX_LEN_DEFAULT;
+	}	
 
 	strncpy( metadata.nombre , config.nombre , MAX_NOMBRE );
 	metadata.nombre[ MAX_NOMBRE ] = '\0';
 
 	if ( get_currentdate( &metadata.fecha , &metadata.hora ) != ST_OK ){
 		print_logs( ERR_GET_DATE );
-		return EXIT_FAILURE;
-	}
-
-	print_headerGPX( files.fout );
-	print_metadataGPX( &metadata , files.fout );
-	print_trk_startGPX( files.fout );
-
-	if ( (*pfunc[ config.protocol ])( &files , config.maxlen ) != ST_OK ){
-
-		print_logs( ERR_PROC_FILE );
-
+		
+		/*cierre de archivos*/
 		if ( files.fin != stdin )
 			fclose( files.fin );
 		if ( files.fout != stdout )
 			fclose( files.fout );
-		if ( files.flog != stderr )
-			close_logger();
+		close_logger();
+		
+		return EXIT_FAILURE;
+	}
+	
+	/* impresion de los headers y demas del gpx */
+	print_headerGPX( files.fout );
+	print_metadataGPX( &metadata , files.fout );
+	print_trk_startGPX( files.fout );
+	
+	/* procesamiento del archivo de entrada */
+	if ( (*pfunc[ config.protocol ])( &files , config.maxlen ) != ST_OK ){
+
+		print_logs( ERR_PROC_FILE );
+		
+		/*cierre de archivos*/
+		if ( files.fin != stdin )
+			fclose( files.fin );
+		if ( files.fout != stdout )
+			fclose( files.fout );
+		close_logger();
 
 		return EXIT_FAILURE;
 	}
-
+	
+	/* impresion de los ends del gpx */
 	print_trk_endGPX( files.fout );
 	print_endGPX( files.fout );
-
+	
+	/*cierre de archivos*/
 	if ( files.fin != stdin )
 		fclose( files.fin );
 	if ( files.fout != stdout )
 		fclose( files.fout );
-	if ( files.flog != stderr )
-		close_logger();
+	close_logger();
 
 	return EXIT_SUCCESS;
 }
